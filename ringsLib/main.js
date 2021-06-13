c37d0d6b1d959cbedba4e72e41526763b31c440b
Lautaro Capella <laucape@gmail.com>
2021-06-13 04:37:50 -0300

    Implemented load()ing feature. Auto-load [ON] Auto-save [ON]

--- ringsLib/main.js
+++ ringsLib/main.js
@@ -1,57 +1,51 @@
+import AppDriver from "./assets/AppDriver.js";
 import Constellation from "./assets/Constellation.js";
 import Ring from "./assets/Ring.js";
 import Task from "./assets/Task.js";
 const { log: cl } = console;
 
-var viaLactea = new Constellation({
-    name: 'Alfa Centauri',
-    rings: [
-        new Ring('day'),
-        new Ring('week'),
-        new Ring('month'),
-        new Ring('year'),
-        new Ring('lifetime')
-    ]
-});
-viaLactea.ring("lifetime")
-    .addTask(new Task({ title: 'Get a plane', tags: ['Car Maintenance'] }))
-    .addTask(new Task({ title: 'Buy new stove', tags: ['Car Maintenance'] }));
-viaLactea.ring("year")
-    .addTask(new Task({ title: 'Build greenhouse', tags: ['Car Maintenance'] }))
-    .addTask(new Task({ title: 'Sell car', tags: ['Car Maintenance'] }));
-viaLactea.ring("month")
-    .addTask(new Task({ title: 'Repair kitchen', tags: ['Home', 'Workshop'] }))
-    .addTask(new Task({ title: 'Get the dog vaccinated', tags: ['Pets'] }))
-    .addTask(new Task({ title: 'Car service at 30.000', tags: ['Car Maintenance'] }));
-viaLactea.ring("week")
-    .addTask(new Task({ title: 'Buy paint for cellar', tags: ['Home', 'Workshop'], metadata: { price: 18241.50, currency: "ARS", qty: 20, um: "L", umISO: "l" } }))
-    .addTask(new Task({ title: 'Repaint cellar' }))
-    .addTask(new Task({ title: 'Withdraw cash from ATM' }))
-    .addTask(new Task({ title: 'Meeting with new Authorities', tags: ['Social'] }));
+var storedData = Constellation.load();
+
+if (storedData) {
+    var App = new AppDriver(storedData);
+
+} else {
+    var App = new AppDriver(
+        new Constellation({
+            name: 'viaLactea',
+            rings: [
+                new Ring('day'),
+                new Ring('week'),
+                new Ring('month'),
+                new Ring('year'),
+                new Ring('lifetime')
+            ]
+        })
+    );
+    App.viaLactea.ring("lifetime")
+        .addTask(new Task({ title: 'Get a plane', tags: ['Car Maintenance'] }))
+        .addTask(new Task({ title: 'Buy new stove', tags: ['Car Maintenance'] }));
+    App.viaLactea.ring("year")
+        .addTask(new Task({ title: 'Build greenhouse', tags: ['Car Maintenance'] }))
+        .addTask(new Task({ title: 'Sell car', tags: ['Car Maintenance'] }));
+    App.viaLactea.ring("month")
+        .addTask(new Task({ title: 'Repair kitchen', tags: ['Home', 'Workshop'] }))
+        .addTask(new Task({ title: 'Get the dog vaccinated', tags: ['Pets'] }))
+        .addTask(new Task({ title: 'Car service at 30.000', tags: ['Car Maintenance'] }));
+    App.viaLactea.ring("week")
+        .addTask(new Task({ title: 'Buy paint for cellar', tags: ['Home', 'Workshop'], metadata: { price: 18241.50, currency: "ARS", qty: 20, um: "L", umISO: "l" } }))
+        .addTask(new Task({ title: 'Repaint cellar' }))
+        .addTask(new Task({ title: 'Withdraw cash from ATM' }))
+        .addTask(new Task({ title: 'Meeting with new Authorities', tags: ['Social'] }));
 
 
-var recoveredJSONObject = {
-    id: 'asereje',
-    creationDate: 19879854133,
-    title: '12.345',
-    tags: [],
-    metadata: { foo: "bar" },
-    ringLog: [],
-    done: true
-};
-viaLactea.ring('day')
-    .addTask(Task.from(recoveredJSONObject));
+    var selectedTask = App.viaLactea.ring("year").tasks[0];
+    App.viaLactea.moveTaskForward(selectedTask);
+    App.viaLactea.moveTaskForward(selectedTask);
+}
 
-// var recoveredJSONObject = {
-//     id: 'anillito',
-//     name: 'El del Capitán Beto',
-//     tasks: []
-// };
-// console.log(recoveredJSONObject);
-// console.log(Ring.from(recoveredJSONObject));
-
+// Rendering to DOM
 // UI
-var v = viaLactea // For the sake of clarity
 var el = document.getElementById("ringsMount");
 // parent.render([...children])
-el.appendChild(v.renderView());
+el.appendChild(App.renderView());
