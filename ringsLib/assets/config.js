2bcdd6af0b78a1f7bfbd6801b325285c907e8a1d
Lautaro Capella <laucape@gmail.com>
2021-06-13 04:25:51 -0300

    Added App and Constellation properties to config

--- ringsLib/assets/config.js
+++ ringsLib/assets/config.js
@@ -1,11 +1,17 @@
 export default {
+    App: {
+        defaultStorageItem: 'ringsStorage'
+    },
+    Constellation: {
+        defaultType: "cons"
+    },
+    Ring: {
+        defaultType: "ring"
+    },
     Task: {
         defaultType: "task",
         defaultTitle: "<Unknown Task>",
         uidLength: 8
-    },
-    Ring: {
-        defaultType: "ring"
     },
     Tag: {
         defaultType: "tag"
